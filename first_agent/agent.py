a1a406e725ba189b9d11d577e7f5f8e22fcaf3a4
Muhammad Umer Amir <161680343+m-umer-dev@users.noreply.github.com>
2026-07-22 00:10:21 +0500

    Learning tool calling

--- first_agent/agent.py
+++ first_agent/agent.py
@@ -1,34 +1,47 @@
-from ollama import chat
+# from ollama import chat
+# from tools import calculator
+
+# messages = []
+
+# print("=== AI Agent ===")
+# print("Type 'exit' to quit.\n")
+
+# while True:
+#     user_input  = input("You: ")
+
+#     if user_input.lower() == "exit":
+#         print("GoodBye")
+#         break
+
+#     messages.append({
+#         "role": "user",
+#         "content": user_input,
+#     })
+
+#     response = chat(
+#         model="qwen2.5:3b",
+#         messages=messages
+#     )
+
+#     ai_reply = response["message"]["content"]
+
+#     messages.append({
+#         "role": "assistant",
+#         "content": ai_reply,
+#     })
+
+#     print("\nAI:", ai_reply)
+#     print()
+
 from tools import calculator
 
-messages = []
+user_input = input("Enter Calculations: ")
+part = user_input.split()
+num1 = int(part[0])
+num2 = int(part[2])
+operation = part[1]
 
-print("=== AI Agent ===")
-print("Type 'exit' to quit.\n")
+result = calculator(num1, num2, operation)
 
-while True:
-    user_input  = input("You: ")
+print("Result:", result)
 
-    if user_input.lower() == "exit":
-        print("GoodBye")
-        break
-
-    messages.append({
-        "role": "user",
-        "content": user_input,
-    })
-
-    response = chat(
-        model="qwen2.5:3b",
-        messages=messages
-    )
-
-    ai_reply = response["message"]["content"]
-
-    messages.append({
-        "role": "assistant",
-        "content": ai_reply,
-    })
-
-    print("\nAI:", ai_reply)
-    print()
--- first_agent/tools.py
+++ first_agent/tools.py
@@ -1,4 +1,4 @@
-def calculator(a, b, operation):
+def calculator(a: float, b: float, operation: str):
 
     if operation == "+":
         return a + b
@@ -10,4 +10,10 @@ def calculator(a, b, operation):
         return a * b
 
     elif operation == "/":
+
+        if b == 0:
+            return "Cannot divide by zero."
+
         return a / b
+
+    return "Invalid operation."
