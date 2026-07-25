dfbca68b0a8baac27c8a98cc048ec15c6112689e
Muhammad Umer Amir <161680343+m-umer-dev@users.noreply.github.com>
2026-07-25 16:18:18 +0500

    Update agent code and create conversation file

--- AI-Mentor-Agent/agent/agent.py
+++ AI-Mentor-Agent/agent/agent.py
@@ -1,28 +1,37 @@
 from ollama import chat
 from config import MODEL_NAME
+from memory.conversation import Conversation
 
 class AIAgent:
 
     def __init__(self):
         self.model = MODEL_NAME
+        self.conversation = Conversation()
 
-    def chat(self,message):
         file_path = "prompts/teacher_prompt.txt"
 
         with open(file_path, "r", encoding="utf-8") as f:
-            system_prompt = f.read()
+            self.system_prompt = f.read()
 
+    def chat(self,user_message):
+
+        self.conversation.add_user_message(user_message)
+
+        messages = [
+            {
+                "role": "system",
+                "content": self.system_prompt
+            },
+            *self.conversation.get_messages()
+        ]
+        
         response = chat(
             model=self.model,
-            messages=[
-                {
-                    "role": "system",
-                    "content": system_prompt
-                },
-                {
-                    "role":"user",
-                    "content": message
-                }
-            ]
+            messages=messages
         )
-        return response.message.content
+
+        ai_reply = response.message.content
+
+        self.conversation.add_ai_message(ai_reply)
+
+        return ai_reply
